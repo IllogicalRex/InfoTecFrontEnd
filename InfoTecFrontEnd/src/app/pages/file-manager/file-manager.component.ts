47794fa442e605b2c80e20c729b1beab3f6c0cbf
Josue <e-jjmartinezt@neoris.com>
2019-12-09 03:14:13 -0700

    Modificacion para el estatus en vista asesor y en vista de seguimient

--- InfoTecFrontEnd/src/app/pages/file-manager/file-manager.component.ts
+++ InfoTecFrontEnd/src/app/pages/file-manager/file-manager.component.ts
@@ -9,18 +9,39 @@ import Swal from 'sweetalert2'
 export class FileManagerComponent implements OnInit {
 
   constructor(public blobStorageService: BlobStorageService) { }
-  files: any;
+  files: string[] = [];  
   fileToUpload: FormData;  
   fileUpload: any;  
+  loadedFile: any;
   fileUpoadInitiated: boolean;  
   fileDownloadInitiated: boolean; 
+  documents: any;
+  id:any;
+  tipo:any;
+  estado:any;
+  statusAceptacion:any=1;
+  statusAnte: any=2;
+  statusPrimer: any=3;
+  statusSegundo: any=4;
+  statusTercer: any=5;
+  statusSeguiRepor: any=6;
+  statusReporResi: any=7;
+  porAprobar: any=1;
+  enProceso: any=2;
+  Aprobado: any=3;
+  Rechazado: any=4;
+  asesor = JSON.parse(localStorage.getItem('token'));
+
+
   ngOnInit() {
-    this.showBlobs();
+    this.id= JSON.parse(localStorage.getItem("token"));
+    this.getDocumentoAlumnoAsesor(this.id.userName);
+    // this.showBlobs();
   }
 //Muestra todos los archivos
   showBlobs() {  
     return this.blobStorageService.showBlobs().subscribe(res=>{
-      this.files = res;  
+      this.documents = res;  
     })
   }  
   //descarga archivo
@@ -30,17 +51,76 @@ export class FileManagerComponent implements OnInit {
 //Elimina un archivo
   deleteFile(fileName: any){
     return Swal.fire({
-      title: 'Eliminar archivo',
-      text: 'Seguro que desea borrar el archivo?',
+      title: 'Reenviar archivo',
+      text: 'Seguro que desea enviar archivo?',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Yes'
     }).then((res)=>{
       if (res.value) {
-        return this.blobStorageService.deleteFile(fileName).subscribe(()=>this.showBlobs())
+        console.log(this.id.userName)
+        return this.blobStorageService.deleteFile(fileName).subscribe(()=>this.getDocumentoAlumnoAsesor(this.id.userName))
       }
       
     })
+  }
+  getDocumentoAlumnoAsesor(id){
+
+    this.blobStorageService.getDocumentoAlumnoAsesor(id).subscribe((res:any)=>{
+          this.documents=res;
+      });
+  }
+  handleFileInput(files: any) {  
+    console.log(files[0].name);
+    this.loadedFile = files;
+  }  
+  uploadFiles(tipo: number, estado:number,comentarioAsesor,comentarioAdmRes,asesor,admin) {
+    let formData: FormData = new FormData(); 
+    let DocName = String(this.loadedFile[0].name);
+    formData.append('asset', this.loadedFile[0], DocName);
+    this.fileToUpload = formData; 
+    this.documents.map((res)=>{
+      console.log(res)
+      if(DocName==res.url){
+        let document = {
+          AlumnId: res.alumnId,
+          Idtipo: tipo,
+          url: res.url,
+          fecha: res.fecha,
+          ComentarioAsesor: res.comentarioAsesor,
+          idEstatus: estado,
+          ComentarioAdmRes: res.comentarioAdmRes,
+          Idasesor: this.asesor.userName,
+          idadmin: res.idadmin
+        };
+        console.log(document)
+          this.deleteFile(res.url);
+          this.onUploadFiles();
+          this.blobStorageService.updateFileToDataBase(document).subscribe();
+      }
+    })
+   
+  }
+
+  onUploadFiles() {  
+    if (this.fileUpoadInitiated) {  
+      return;  
+    }  
+    this.fileUpoadInitiated = true;  
+    if (this.fileToUpload == undefined) {  
+      this.fileUpoadInitiated = false;  
+      return false;  
+    } else {
+      return  this.blobStorageService.insertFile(this.fileToUpload).subscribe(res=>res);
+    }
+  }
+
+  setTipo(value){
+    this.tipo=value;
+  }
+  setEstado(value){
+    console.log(value)
+    this.estado=value;
   }
 
 }
--- InfoTecFrontEnd/src/app/pages/seguimiento/seguimiento.component.ts
+++ InfoTecFrontEnd/src/app/pages/seguimiento/seguimiento.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 import { BlobStorageService } from '../../services/BlobStorageService.service';
 import { FileUploadService } from 'src/app/services/file-upload.service';
-
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-seguimiento',
   templateUrl: './seguimiento.component.html',
@@ -15,7 +16,20 @@ export class SeguimientoComponent implements OnInit {
   loadedFile: any;
   fileUpoadInitiated: boolean;  
   fileDownloadInitiated: boolean; 
+  documents: any;
   user = JSON.parse(localStorage.getItem('token'));
+  statusAceptacion:any=1;
+  statusAnte: any=2;
+  statusPrimer: any=3;
+  statusSegundo: any=4;
+  statusTercer: any=5;
+  statusSeguiRepor: any=6;
+  statusReporResi: any=7;
+  estado:any;
+  porAprobar: any=1;
+  enProceso: any=2;
+  Aprobado: any=3;
+  Rechazado: any=4;
 
   constructor(public blobStorageService: BlobStorageService,
               public fileService: FileUploadService) { 
@@ -23,7 +37,7 @@ export class SeguimientoComponent implements OnInit {
   ngOnInit() {
 
     let id= JSON.parse(localStorage.getItem("token"));
-   this.getDocument(id.userName);
+    this.getDocument(id.userName);
   }
   //maneja el tipo de archivo
   handleFileInput(files: any) {  
@@ -40,34 +54,65 @@ export class SeguimientoComponent implements OnInit {
       this.fileUpoadInitiated = false;  
       return false;  
     } else {
-      return  this.blobStorageService.insertFile(this.fileToUpload).subscribe(res=>res);
+      return  this.blobStorageService.insertFile(this.fileToUpload).subscribe();
     }
   }
 
-  uploadFiles(tipo: number) {
+  uploadFiles(tipo: number, estado:number,comentarioAsesor,comentarioAdmRes,asesor,admin) {
     console.log('archivo  ',  this.user.userName);
     let DocName = String(this.user.userName + '-' + this.loadedFile[0].name);
     let formData: FormData = new FormData(); 
     formData.append('asset', this.loadedFile[0], DocName);
     this.fileToUpload = formData; 
     this.onUploadFiles();
-    let docuemnt = {
+    let document = {
       AlumnId: this.user.userName,
       Idtipo: tipo,
       url: DocName,
       fecha: new Date(),
-      ComentarioAsesor: '',
-      idEstatus: 1,
-      ComentarioAdmRes: '',
+      ComentarioAsesor: 'ComentarioAsesor',
+      idEstatus: estado,
+      ComentarioAdmRes: 'comentarioAdmRes',
       Idasesor: '12345678',
       idadmin: '15327486'
     };
-    this.blobStorageService.addFileToDataBase(docuemnt).subscribe();
+    this.blobStorageService.addFileToDataBase(document).subscribe((res:any)=>{
+        Swal.fire({
+          title: 'Archivo enviado!',
+          text: '',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        })
+           return res;
+    });
   }
 
   getDocument(numControl){
-    console.log(numControl)
-    this.blobStorageService.getDocument(numControl).subscribe();
+    this.blobStorageService.getDocument(numControl).subscribe((res:any)=>{
+        res.map((res: any)=>{
+          if(res.idEstatus!=3){
+            if(res.idtipo===1){
+              console.log(res)
+              this.documents=res;
+            }
+            if(res.idtipo===2){
+              console.log(res)
+              this.documents=res;
+            }
+            if(res.idtipo===4){
+              console.log(res)
+              this.documents=res;
+            }
+          }
+          if(res.idEstatus==3){
+            this.documents={
+              idtipo:res.idtipo+1,
+              idEstatus:1
+            };
+          }
+        })
+      });
   }
+
 
 }
